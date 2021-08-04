64df1ce8365a3f2bafc858a377d9d92546a568e3
Elias Peeters <Elias.peeters@otto.de>
2021-08-04 17:43:07 +0200

    Added test article and catch statement

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,4 +1,10 @@
+const fs = require('fs')
+
 app.get('/article/:name', function(req, res) {
+    try {
+        fs.readFileSync(`views/${req.params.name}.ejs`)
+    } catch {
+        res.send('404: That article does not exist')
+    }
     res.render(req.params.name)
-    //res.send(req.params.name)
 })
