07bd3d387737684406ad523616157610b185b82e
Elias Peeters <Elias.peeters@otto.de>
2021-08-05 13:12:12 +0200

    Added new article structure

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,15 +1,17 @@
+const { RSA_NO_PADDING } = require('constants');
 const fs = require('fs');
 let attributesRoute = require('./attributesRoute')
 
 app.get('/article/:name', function(req, res) {
-    try {
-        fs.readFileSync(`views/${req.params.name}.ejs`)
-    } catch {
-        res.send('404: That article does not exist')
-    }
     let attributes = attributesRoute.getAttributesOneArticle(req.params.name);
+    if (attributes == 'Article does not exist') {
+        res.send(attributes);
+        return;
+    }
+
     let header = `<p class="date">${attributes.date}</p>
     <h1>${attributes.heading}</h1>
     `
-    res.render(req.params.name, {header: header})
+    res.render('article', {header: header, articleName: req.params.name})
+    // res.render(req.params.name, {header: header})
 })
