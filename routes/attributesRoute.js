ef9647896c8c3ad598d15d2c666aa72a9c3f7823
Elias Peeters <Elias.peeters@otto.de>
2021-08-05 15:33:38 +0200

    Added a lot and a lot more

--- routes/attributesRoute.js
+++ routes/attributesRoute.js
@@ -1,20 +1,65 @@
 const fs = require('fs')
 
-function getAttributes() {
-    return JSON.parse(fs.readFileSync('assets/attributes.json'))
+function getAttributes(server = 'external', sort = 'date') {
+    let serverAdress = server == 'external'? 'http://185.183.157.185:8083':'http://localhost:8083'
+
+    let attributes = JSON.parse(fs.readFileSync('assets/attributes.json'))
+    let i = attributes.articles.length;
+    while (i--) {
+        if (attributes.articles[i].visible == false) {
+            attributes.articles.splice(i, 1)
+        }
+    }
+
+    for (article in attributes) {
+        attributes[article].image =  serverAdress + '/assets/' +attributes[article].image;
+    }
+    if (sort == 'date') {
+        attributes.articles.sort((a, b) => (a.dateUTC < b.dateUTC) ? 1 : -1);
+    } else if (sort == 'date-1') {
+        attributes.articles.sort((a, b) => (a.dateUTC < b.dateUTC) ? 1 : -1).reverse();
+    } else if (sort == 'name') {
+        attributes.articles.sort(function (a, b) {
+            if (a.heading < b.heading) {retrun -1}
+            if (a.heading > b.heading) {return 1}
+            return 0
+        })
+    } else if (sort == 'name-1') {
+        attributes.articles.sort(function (a, b) {
+            if (a.heading < b.heading) {retrun -1}
+            if (a.heading > b.heading) {return 1}
+            return 0
+        }).reverse()
+    }
+    
+
+    return attributes
 }
 
 function getAttributesOneArticle(name) {
     let attributes = getAttributes();
-    if (attributes[name] == null) {
+    const position = attributes.articles.indexOf(name);
+
+    let articlePostionInArray = -1
+    attributes.articles.forEach(function(element, index){
+        if (element.name == name) {
+            articlePostionInArray = index
+        }
+    });
+    console.log(articlePostionInArray)
+    // console.log(attributes.articles)
+    if (articlePostionInArray == -1) {
         return 'Article does not exist';
     } else {
-        return attributes[name]
+        return attributes.articles[articlePostionInArray]
     }
 }
 
 app.get('/attributes', function(req, res) {
-    let attributes = getAttributes()
+    let server = req.query.server == 'internal' ? 'internal':'external';
+    let sort = req.query.sort == null ? 'date':req.query.sort;
+    
+    let attributes = getAttributes(server, sort)
     res.send(attributes)  
 })
 
--- websiteblogbackend.js
+++ websiteblogbackend.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 app = express();
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/assets', express.static('assets'))
 
 // routes
